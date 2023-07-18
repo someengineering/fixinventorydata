07d2e2bc9dad38954eab4594618dca8669585c7b
Lukas Lösche <lukas@some.engineering>
2023-07-18 19:56:24 +0200

    Bump 0.2.0

--- resotodata/__init__.py
+++ resotodata/__init__.py
@@ -2,7 +2,7 @@
 Miscellaneous Resoto Data
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Miscellaneous Resoto data.
-:copyright: © 2022 Some Engineering Inc.
+:copyright: © 2023 Some Engineering Inc.
 :license: Apache 2.0, see LICENSE for more details.
 """
 
@@ -10,5 +10,5 @@ __title__ = "resotodata"
 __description__ = "Miscellaneous Resoto data."
 __author__ = "Some Engineering Inc."
 __license__ = "Apache 2.0"
-__copyright__ = "Copyright © 2022 Some Engineering Inc."
-__version__ = "0.1.0"
+__copyright__ = "Copyright © 2023 Some Engineering Inc."
+__version__ = "0.2.0"
